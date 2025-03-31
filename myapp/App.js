5a25bec093ea570f32bf4d6010b9015446a37b19
Yorke Ama Amppiah Melody <melodyyorke4@gmail.com>
2025-03-31 11:39:50 +0000

    Update App.js

--- myapp/App.js
+++ myapp/App.js
@@ -1,3 +1,31 @@
-import {View,Image,Text} from 'react-native';
-const logoImg = require('./logo.png');
+import {View,Image,Text,StyleSheet} from 'react-native';
+const logoImg = require('./assets/Card.png');
+import profile from './assets/profile.png';
+import search from './assets/search.png';
 
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+        <View style={{backgroundColor:'white',top:55}}>
+         <Text style={{color:'black',left:86,fontWeight:200}}>Welcome back,</Text>   
+         <Text style={{color:'black',top:10,left:90,fontWeight:600,fontSize:18}}>Eric Atsu</Text>  
+         </View>
+      
+          {/* search Image View*/}
+          <Image source={search} style={{left:300,top:30}}></Image>
+         {/* profile Image View*/}
+          <Image source={profile} style={{left:25}}></Image>
+          {/* logo Image View*/}
+          <Image source={logoImg} style={{left:20,top:20}}></Image>
+        
+
+        </View>
+        );
+        }
+    
+       const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          backgroundColor: '#fff'
+        }
+        });
