504cf2881e1cb510f1afb8b3ff75dd0458d11bf3
Yorke Ama Amppiah Melody <melodyyorke4@gmail.com>
2025-04-01 13:09:48 +0000

    Update App.js

--- myapp/App.js
+++ myapp/App.js
@@ -2,8 +2,12 @@ import {View,Image,Text,StyleSheet} from 'react-native';
 const logoImg = require('./assets/Card.png');
 import profile from './assets/profile.png';
 import search from './assets/search.png';
+import send from './assets/send.png'
+import recieve from './assets/recieve.png'
+import loan from './assets/loan.png'
+import topUp from './assets/topUp.png'
 
-export default function HomeScreen() {
+export default function HomeScreen(){
     return (
         <View style={styles.container}>
         <View style={{backgroundColor:'white',top:55}}>
@@ -17,9 +21,32 @@ export default function HomeScreen() {
           <Image source={profile} style={{left:25}}></Image>
           {/* logo Image View*/}
           <Image source={logoImg} style={{left:20,top:20}}></Image>
-        
 
-        </View>
+         <View>
+          {/* send Image View*/}
+          <Image source={send} style={{left:40,top:65}}></Image>
+          <Text style={{left:33,top:75}}>Sent</Text>
+          </View>
+         
+          <View>
+            {/* recieve Image View*/}
+            <Image source={recieve} style={{left:123,top:30}}></Image>
+            <Text style ={{left:110,top:40}}>Recieve</Text>
+          </View>
+
+          <View>
+            {/* loan Image View*/}
+            <Image source={loan} style={{left:200}}></Image>
+            <Text style={{left:200,top:10}}>loan</Text>
+          </View>
+
+          <View>
+            {/* topUp Image View*/}
+            <Image source={topUp} style={{left:300}}></Image>
+            <Text style={{left:300}}>TopUp</Text>
+            </View>
+            </View>
+        
         );
         }
     
