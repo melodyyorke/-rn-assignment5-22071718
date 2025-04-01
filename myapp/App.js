6247f5244aac5e28b813aec1ed67651c8e84ef5f
Yorke Ama Amppiah Melody <melodyyorke4@gmail.com>
2025-04-01 19:28:38 +0000

    Update App.js

--- myapp/App.js
+++ myapp/App.js
@@ -1,69 +1,62 @@
-import {View,Image,Text,StyleSheet} from 'react-native';
+import {View,Text,Image,StyleSheet,ScrollView} from 'react-native';
 const logoImg = require('./assets/Card.png');
-import profile from './assets/profile.png';
-import search from './assets/search.png';
-import send from './assets/send.png';
-import recieve from './assets/recieve.png';
-import loan from './assets/loan.png';
-import topUp from './assets/topUp.png';
+import profile from'./assets/profile.png';
+import search from'./assets/search.png';
 
 export default function HomeScreen(){
     return (
-      <View style= {styles.HomeScreen}>
-        <View style={{backgroundColor:'white'}}>
-         <Text style={{color:'black',left:86,fontWeight:200,top:30}}>Welcome back,</Text>   
-         <Text style={{color:'black',top:30,left:90,fontWeight:600,fontSize:18}}>Eric Atsu</Text>  
-        
-        {/* profile Image View*/}
-        <Image source={profile} style={{left:25}}></Image>
-         
-          {/* search Image View*/}
-          <Image source={search} style={{left:300}}></Image>
+      <View style={styles. container}>
+        <ScrollView>
+        <View style={styles.header}>
 
-          {/* logo Image View*/}
-          <Image source={logoImg} style={{left:20}}></Image>
-  
-           <View>
-          {/* send Image View*/}
-          <Image source={send} style={{left:40,top:65}}></Image>
-          <Text style={{left:33,top:75}}>Sent</Text>
-          </View>
-         
-          <View>
-            {/* recieve Image View*/}
-            <Image source={recieve} style={{left:123,top:30}}></Image> 
-            <Text style ={{left:110,top:40}}>Recieve</Text>
+           {/* profile,View Text,search */} 
+           <View style={styles.profileView}>
+          <Image source={profile} style={styles.profileImage}/>
+        </View>
+
+          <View style={{right:60,gap:7}}>
+            <Text style={{fontWeight:200}}> Welcome back, </Text>
+            <Text style={styles.mainText}> Eric Atsu</Text>
           </View>
 
           <View>
-            {/* loan Image View*/}
-            <Image source={loan} style={{left:200}}></Image>
-            <Text style={{left:200}}>loan</Text>
-          </View>
+            <Image source={search}/>
+            <Image style={styles.search}></Image>
 
-          <View>
-            {/* topUp Image View*/}
-            <Image source={topUp} style={{left:300}}></Image>
-            <Text style={{left:300}}>TopUp</Text>
+              </View>
             </View>
-            
-            <Text style={{left:30,top:5,fontWeight:600,fontSize:20}}>Transaction</Text>
-            <Text style={{left:300,color:'blue'}}>Sell All</Text>
-            </View>
-            </View>
-       
-  
-        
-        );
-        }
-    
-       const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-          backgroundColor: '#fff'
-          },
-          
-              
+            </ScrollView>
+
+      </View>
+    )
 }
- );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+   paddingTop:30,
+   paddingLeft:10,
+    marginLeft:5,
+    marginRight:5
+    },
+    header: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: 10,
+      marginRight:10,
+      backgroundColor: '#fff',
+      alignItems:'center',
+      display:'flex'
+      },
+    
+    mainText: {
+    fontSize: 18,
+    fontWeight: 'bold'
+    },
+
+
+    
+  } 
+  )
 
