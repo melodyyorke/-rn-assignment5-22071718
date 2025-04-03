1e39e2e5ef7123763654d59464351317b8e7543b
Yorke Ama Amppiah Melody <melodyyorke4@gmail.com>
2025-04-03 22:21:49 +0000

    main task

--- myapp/App.js
+++ myapp/App.js
@@ -1,4 +1,4 @@
-import {View,Text,Image,StyleSheet,ScrollView, Button,} from 'react-native';
+import {View,Text,Image,StyleSheet,ScrollView, Button, ImageBackground,} from 'react-native';
 const logoImg = require('./assets/Card.png');
 import profile from'./assets/profile.png';
 import search from'./assets/search.png';
@@ -18,9 +18,10 @@ import settings from './assets/settings.png';
 
 export default function HomeScreen(){
     return (
+      <ScrollView>
       <View style={styles. container}>
         <View style={styles.header}>
-
+          
            {/* profile,View Text,search */} 
            <View style={styles.profileView}>
           <Image source={profile} style={styles.profileImage}/>
@@ -36,7 +37,7 @@ export default function HomeScreen(){
              </View>
             </View>
             {/* card Image View*/}
-            <Image source={card} style={{top:5,marginLeft:5,marginRight:15}}></Image>
+            <Image source={card} style={{top:25,marginLeft:5,marginRight:15}}></Image>
 
             <View style={styles.cardView}>
               {/* send,recieve,loan,topUp,View Text */}
@@ -58,12 +59,12 @@ export default function HomeScreen(){
                </View>
                
                <View>
-                  <Text style={{top:30,margin:5}}>
+                  <Text style={{top:55,margin:5}}>
                     <View>
-               <Text style={{left:5}}>Sent</Text>
+               <Text style={{left:6}}>Sent</Text>
                </View>
                <View>
-               <Text style={{left:60}}>Receive</Text>
+               <Text style={{left:62}}>Receive</Text>
                </View>
                <View>
                <Text style={{left:120}}>Loan</Text>
@@ -75,62 +76,74 @@ export default function HomeScreen(){
                 </View>
 
                 <View>
-                  <Text style={{top:60,marginRight:5,marginLeft:15}}>
-                    <Text>Transaction</Text>
+                  <Text style={{top:70,marginLeft:5}}>
+                    <Text style={{fontWeight:600,fontSize:20,left:150}}>Transaction</Text>
                     <View>
-                    <Text style={{color:'blue',left:200}}>Sell All</Text>
+                    <Text style={{color:'blue',left:170}}>Sell All</Text>
                     </View>
                    
                   </Text>
                 </View>
               
 
-            <View style={styles.cardView2}>
-              {/* apple,spotify,moneyTransfer,grocery*/}
-              <View style={{borderRadius:20,backgroundColor:'lightgrey',width:40,height:40,alignItems:'center',justifyContent:'center'}}>
-                <Image source={apple} style={styles.appleImage}></Image>
+              {/* apple*/}
+              <Text style={{left:60,fontSize:20,fontWeight:500,top:80}}>Apple Store</Text>
+              <Text style={{left:275,fontSize:15,fontWeight:700,top:60}}>-$5,99</Text>
+              <Text style={{left:60,top:63,fontWeight:200}}>Entertainment</Text>
+              <View style={{borderRadius:30,backgroundColor:'lightgrey',width:45,height:45,alignItems:'center',justifyContent:'center',top:20}}>
+              <Image source={apple} style={styles.appleImage}></Image>
+                 </View>
+
+              {/*spotify*/}
+              <Text style={{left:60,fontSize:20,fontWeight:500,top:60}}>Sportify</Text>
+              <Text style={{left:270,fontSize:15,fontWeight:700,top:40}}>-$12,99</Text>
+              <Text style={{left:60,top:43,fontWeight:200}}>Music</Text>
+              <View style={{borderRadius:30,backgroundColor:'lightgrey',width:45,height:45,alignItems:'center',justifyContent:'center',top:5}}>
+              <Image source={spotify} style={styles.spotifyImage}></Image>
+                 </View>
+
+               {/*moneyTransfer*/}
+               <Text style={{left:60,fontSize:20,fontWeight:500,top:60}}>Money Transfer</Text>
+              <Text style={{left:290,fontSize:15,fontWeight:700,top:40,color:'blue'}}>$300</Text>
+              <Text style={{left:60,top:43,fontWeight:200}}>Music</Text>
+              <View style={{borderRadius:30,backgroundColor:'lightgrey',width:45,height:45,alignItems:'center',justifyContent:'center'}}>
+              <Image source={moneyTransfer} style={styles.moneyTransfer}></Image>
+                 </View>
+
+              
+               {/*grocery*/}
+               <Text style={{left:60,fontSize:20,fontWeight:500,top:45}}>Grocery</Text>
+              <Text style={{left:290,fontSize:15,fontWeight:700,top:25}}>-$88</Text>
+              <View style={{borderRadius:30,backgroundColor:'lightgrey',width:45,height:45,alignItems:'center',justifyContent:'center'}}>
+              <Image source={grocery} style={styles.groceryImage}></Image>
+                 </View>
+
+                 <View style= {styles.cardView2}>
+                  {/* home,myCard,statictics,settings*/}
+                  <View>
+                    <Image source={home} style={styles.homeImage}></Image>
+                    <Text style={{right:10,top:15,color:'blue'}}>Home</Text>
+                    </View>
+                    <View>
+                      <Image source={myCards} style={styles.myCardsImage}></Image>
+                      <Text style={{right:15,top:15}}>My Cards</Text>
+                      </View>
+                      <View>
+                        <Image source={statictics} style={styles.staticticsImage}></Image>
+                        <Text style={{right:20,top:15}}>Statistics</Text>
+                        </View>
+                        <View>
+                          <Image source={settings} style={styles.settingsImage}></Image>
+                          <Text style={{right:10,top:15,color:'blue'}}>Settings</Text>
+                          
+                          </View>
+                        
+                              
+ </View>               
+                   
+  </View>  
+  </ScrollView>                  
       
-              </View>
-            
-              <View style={{borderRadius:20,backgroundColor:'lightgrey',width:40,height:40,alignItems:'center',justifyContent:'center'}}>
-                <Image source={spotify} style={styles.spotifyImage}></Image>
-              </View>
-              <View style={{borderRadius:20,backgroundColor:'lightgrey',width:40,height:40,alignItems:'center',justifyContent:'center'}}>
-                <Image source={moneyTransfer} style={styles.moneyTransferImage}></Image>
-              </View>
-              <View style={{borderRadius:20,backgroundColor:'lightgrey',width:40,height:40,alignItems:'center',justifyContent:'center'}}>
-                <Image source={grocery} style={styles.groceryImage}></Image>
-                </View>
-            </View>
-  
-
-
-          
-            <View style= {styles.cardView3}>
-              {/* home,myCard,statictics,settings*/}
-              <View>
-                <Image source={home} style={styles.homeImage}></Image>
-              </View>
-              <View>
-                <Image source={myCards} style={styles.myCardsImage}></Image>
-              </View>
-              <View>
-                <Image source={statictics} style={styles.staticticsImage}></Image>
-              </View>
-              <View>
-                <Image source={settings} style={styles.settingsImage}></Image>
-              </View>
-               </View>
-
-               <View>
-                < View style={styles.header2}>
-                <Text style={styles.headerText}>Settings</Text>
-                </View>
-               </View>
-
- </View>
-
-        
     );
 }
 
@@ -138,7 +151,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-   paddingTop:20,
+   paddingTop:50,
    paddingLeft:10
     },
     header: {
@@ -171,7 +184,7 @@ const styles = StyleSheet.create({
     cardView: {
       flexDirection: 'row',
       justifyContent: 'space-around',
-      top:25,
+      top:50,
       display:'flex',
       marginRight:15,
       marginLeft:5
@@ -179,35 +192,14 @@ const styles = StyleSheet.create({
   
      },
 
-
      cardView2: {
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-      top:60,
-      alignItems:'flex',
-      display:'flex',
-      width:250,
-      height:250,
-      marginRight:5,
-      marginLeft:15
-     },
-     cardView3: {
       flexDirection: 'row',
       justifyContent: 'space-around',
-      top:10,
-      alignItems:'flex',
       display:'flex',
-      paddingTop:10,
-      width:250,
-      height:50,
-      backgroundColor:'#fff',
-      borderRadius:0,
-      alignItems:'baseline',
-      marginRight:5,
-      marginLeft:15
-      }
 
-    
+
+     }
+
     
     
   } 
